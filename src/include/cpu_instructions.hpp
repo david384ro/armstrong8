ca89b07a90d272b7e55a41f547ef8b913bdc26ee
david384ro <davidul384@gmail.com>
2024-08-08 21:30:41 +0300

    stuff

--- src/include/cpu_instructions.hpp
+++ src/include/cpu_instructions.hpp
@@ -1,44 +1,44 @@
 enum Instructions {
-    // Miscellaneous Instructions: 0x00 - 0x0F
-    BRK              = 0x01, // Break (Interrupt)
-    NOP              = 0xEA, // No Operation
+    // Miscellaneous Instructions
+    BRK,             // Break (Interrupt)
+    NOP,             // No Operation
 
-    // Jump Instructions: 0x40 - 0x4F
-    JMP              = 0x4C, // Jump to Address
+    // Jump Instructions
+    JMP,             // Jump to Address
 
-    // Arithmetic Instructions: 0x60 - 0x6F
-    ADD              = 0x60, // Add
-    SUB              = 0x61, // Subtract
-    MUL              = 0x62, // Multiply
-    CMP              = 0x63, // Compare
-    CMX              = 0x64, // Compare X
+    // Arithmetic Instructions
+    ADD,             // Add
+    SUB,             // Subtract
+    MUL,             // Multiply
+    CMP,             // Compare
+    CMX,             // Compare X
 
-    // Store Instructions: 0x80 - 0x8F
-    STA_ZERO_PAGE    = 0x85, // Store Accumulator in Zero Page
-    STA_NEXT_PAGE    = 0x86, // Store Accumulator in Next Page
-    STAX_ZERO_PAGE   = 0x89, // Store Accumulator in Zero Page by an X offset
-    STAX_NEXT_PAGE   = 0x88, // Store Accumulator in Next Page by an X offset
+    // Store Instructions
+    STA_ZERO_PAGE,   // Store Accumulator in Zero Page
+    STA_NEXT_PAGE,   // Store Accumulator in Next Page
+    STAX_ZERO_PAGE,  // Store Accumulator in Zero Page by an X offset
+    STAX_NEXT_PAGE,  // Store Accumulator in Next Page by an X offset
 
-    // Transfer Instructions: 0x90 - 0x9F
-    TAX              = 0xAA, // Transfer Accumulator to X Register
-    TAY              = 0xA8, // Transfer Accumulator to Y Register
-    TXA              = 0x8A, // Transfer X Register to Accumulator
-    TYA              = 0x98, // Transfer Y Register to Accumulator
-    TXY              = 0x99, // Transfer X Register to Y Register
-    TYX              = 0xFE, // Transfer Y Register to X Register
+    // Transfer Instructions
+    TAX,             // Transfer Accumulator to X Register
+    TAY,             // Transfer Accumulator to Y Register
+    TXA,             // Transfer X Register to Accumulator
+    TYA,             // Transfer Y Register to Accumulator
+    TXY,             // Transfer X Register to Y Register
+    TYX,             // Transfer Y Register to X Register
 
-    // Load Instructions: 0xA0 - 0xAF
-    LDA_IMMEDIATE    = 0xA9, // Load Accumulator with Immediate
-    LDA_ZERO_PAGE    = 0xA5, // Load Accumulator with Zero Page
-    LDA_NEXT_PAGE    = 0xA6, // Load Accumulator with Next Page
+    // Load Instructions
+    LDA_IMMEDIATE,   // Load Accumulator with Immediate
+    LDA_ZERO_PAGE,   // Load Accumulator with Zero Page
+    LDA_NEXT_PAGE,   // Load Accumulator with Next Page
 
-    // Increment/Decrement Instructions: 0xE0 - 0xEF
-    INX              = 0xE8, // Increment X Register
-    DEX              = 0xCA, // Decrement X Register
+    // Increment/Decrement Instructions
+    INX,             // Increment X Register
+    DEX,             // Decrement X Register
 
-    // Branch Instructions: 0xD0 - 0xDF
-    BEQ              = 0xF0, // Branch if Equal
-    BNE              = 0xD0, // Branch if Not Equal
-    BPL              = 0x10, // Branch if Plus
-    BMI              = 0x30, // Branch if Minus
+    // Branch Instructions
+    BEQ,             // Branch if Equal
+    BNE,             // Branch if Not Equal
+    BPL,             // Branch if Plus
+    BMI,             // Branch if Minus
 };
